75e506202c045baed08dccce6ece9f71d5f36c4f
Taesoo Kim <taesoo@mit.edu>
2014-05-20 19:24:01 -0400

    diff

---
+++ bin/diff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HERE=$(pwd)
+TMPD=$(mktemp -d /tmp/latexdiff.XXXXXXXXXX)
+NEWD=$(pwd)
+OLDD=$TMPD/old
+
+if [[ ! -e $HERE/.git ]]; then
+  echo "please run on git repo"
+  exit 1
+fi
+
+git clone $NEWD $OLDD
+
+cd $OLDD
+git checkout $1
+latexdiff --flatten p.tex $NEWD/p.tex > diff.tex
+
+make MAIN=diff
+cp -f diff.pdf $NEWD/diff.pdf
